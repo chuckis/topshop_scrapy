21209788f3441ae15f2f6ec86f4c49694f6b4727
chuckis <chuckis94@gmail.com>
2020-02-25 00:38:08 +0200

    logging in parse_categories

--- topshop/spiders/basic.py
+++ topshop/spiders/basic.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-#urls = response.css('ul.MegaNav-categories > li > a::attr(href)').extract()
 
 class BasicSpider(scrapy.Spider):
     name = 'basic'
-    allowed_domains = ['https://www.topshop.com/']
-    start_urls = ['http://https://www.topshop.com//']
+    allowed_domains = ['www.topshop.com']
+    start_urls = ['https://www.topshop.com//']
 
     def parse(self, response):
-        pass
+        urls = response.css('ul.MegaNav-categories > li > a::attr(href)').extract()
+
+        for url in urls:
+            yield response.follow(url, callback=self.parse_categories)
+
+    def parse_categories(self, response):
+        urls = response.css('nav.MegaNav-subNav > a::attr(href)').extract()
+
+        for url in urls:
+            yield response.follow(url, callback=self.parse_product_list)
+
+    def parse_product_list(self, response):
+        # products = response.css('div.ProductList-products > div > a::attr(href)').extract()
+
+        self.logger.info("Visited %s", response.url)
+
+        # for product in products:
+        #     yield response.follow(product, callback=self.parse_product_detail)
+
+    # def parse_product_detail(self, response):
+    #     product_name = response.css('h1.ProductDetail-title ::text').get()
+    #     print (product_name)
