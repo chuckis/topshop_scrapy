c6ad0a3c7d133c8487db15f6eadabeaf180195fa
chuckis <chuckis94@gmail.com>
2020-02-27 00:49:38 +0200

    gets product name

--- topshop/spiders/basic.py
+++ topshop/spiders/basic.py
@@ -9,24 +9,15 @@ class BasicSpider(scrapy.Spider):
 
     def parse(self, response):
         urls = response.css('ul.MegaNav-categories > li > a::attr(href)').extract()
-
-        for url in urls:
-            yield response.follow(url, callback=self.parse_categories)
-
-    def parse_categories(self, response):
-        urls = response.css('nav.MegaNav-subNav > a::attr(href)').extract()
-
         for url in urls:
             yield response.follow(url, callback=self.parse_product_list)
 
     def parse_product_list(self, response):
-        # products = response.css('div.ProductList-products > div > a::attr(href)').extract()
-
-        self.logger.info("Visited %s", response.url)
-
-        # for product in products:
-        #     yield response.follow(product, callback=self.parse_product_detail)
-
-    # def parse_product_detail(self, response):
-    #     product_name = response.css('h1.ProductDetail-title ::text').get()
-    #     print (product_name)
+        products = response.css('a.Product-nameLink::attr(href)').extract()
+        #self.logger.info("Visited %s", response.url)
+        for product in products:
+            yield response.follow(product, callback=self.parse_product_detail)
+         
+    def parse_product_detail(self, response):
+        product_name = response.css('h1.ProductDetail-title ::text').get()
+        print (product_name)
